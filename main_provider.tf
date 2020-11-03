357727c1af666fb2878e2ff0c44cdc2eddfd15de
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2020-11-03 13:55:47 +0000

    refactor: moved tf provider to dedicated file

---
+++ main_provider.tf
@@ -0,0 +1,25 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+/*
+terraform {
+  backend "remote" {
+    organization = "dark-contoso"
+
+    workspaces {
+      name = "terraform-development"
+    }
+  }
+}
+*/
