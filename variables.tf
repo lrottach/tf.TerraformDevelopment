7db110689b3c57960c1eedce2bd700ff942f06b8
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2020-11-03 16:04:47 +0000

    feat: added tags to variables

--- variables.tf
+++ variables.tf
@@ -3,3 +3,12 @@ variable "resource_location" {
   description = "The Azure region used to deploy resources"
   default = "switzerlandnorth"
 }
+
+variable "resource_tags" {
+  type = map
+  default = {
+    Environment = "Production"
+    CreatedBy = "lrottach@darkcontoso.io"
+    CreatedOn = "03/11/2020"
+  }
+}
